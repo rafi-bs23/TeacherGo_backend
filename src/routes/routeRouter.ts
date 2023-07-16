25d2b6e21ee320ad890f90175215eb89c6a9b7d8
rafi-bs23 <tanvir.hasan@brainstation-23.com>
2023-07-16 23:32:55 +0600

    get all route added to routeRouter

--- src/routes/routeRouter.ts
+++ src/routes/routeRouter.ts
@@ -4,6 +4,7 @@ import * as routeController from '../controller/routeController';
 const router = express.Router();
 
 router.post('/', routeController.createRoute);
+router.get('/',routeController.getAllRoute)
 router.patch('/:id', routeController.updateRoute);
 router.get('/drivers-and-teachers', routeController.getAllDriverAndTeacher);
 
