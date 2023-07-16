5d03eb66316c080970738cc60f38a6df9af211d5
rafi-bs23 <tanvir.hasan@brainstation-23.com>
2023-07-17 03:14:56 +0600

    ..

--- src/controller/routeController.ts
+++ src/controller/routeController.ts
@@ -41,7 +41,7 @@ export const updateRoute = catchAsync(
 
 export const getAllRoute = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const routes: IRoute[] | null = await RouteModel.find();
+    const routes: IRoute[] | null = await RouteModel.find().populate('driver');
     res.status(200).json({
       status: 'success',
       result: routes ? routes.length : 0,
