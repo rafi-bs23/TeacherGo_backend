ee040d2977cea765f61bffa1a7fd5e07ce11603e
rafi-bs23 <tanvir.hasan@brainstation-23.com>
2023-07-17 02:18:17 +0600

    fix a bug

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -12,7 +12,7 @@ router.get('/teachers', teacherController.getAllTeacher);
 router.get('/drivers', driverController.getAllDrivers);
 
 router.patch('/update-user-info/:id', authController.updateUserInfo);
-router.get('get-user/:id', authController.getUser);
+router.get('/get-user/:id', authController.getUser);
 
 router.patch('/teacher/:id', teacherController.updateTeacherStatus);
 router.patch('/driver/:id', driverController.updateDriverStatus);
